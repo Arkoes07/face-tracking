0d5a59390c4f31164a9ee6e1a24852cc93f950bf
Arkoes07 <alwiesukra@gmail.com>
2020-06-10 09:01:57 +0700

    project template

---
+++ FaceTracking.cpp
@@ -0,0 +1,15 @@
+﻿// FaceTracking.cpp : Defines the entry point for the application.
+//
+
+#include "FaceTracking.h"
+
+#include <opencv2/opencv.hpp>
+#include <dlib/image_processing.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
