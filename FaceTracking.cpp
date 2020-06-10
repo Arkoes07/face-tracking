8a3ec23a938659ff9e262b100d6872c75836ccc3
Arkoes07 <alwiesukra@gmail.com>
2020-06-10 09:17:33 +0700

    Read and display frame from camera

--- FaceTracking.cpp
+++ FaceTracking.cpp
@@ -1,15 +1,38 @@
 ﻿// FaceTracking.cpp : Defines the entry point for the application.
 //
 
-#include "FaceTracking.h"
-
 #include <opencv2/opencv.hpp>
-#include <dlib/image_processing.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	// open camera
+	cv::VideoCapture cap(0);
+
+	// current frame container
+	cv::Mat currentFrame;
+
+	// get frames from camera
+	while (1) {
+
+		// read current frame
+		cap.read(currentFrame);
+
+		// Display current frame
+		cv::imshow("Frame", currentFrame);
+
+		// Press ESC on keyboard to exit
+		char c = (char)cv::waitKey(25);
+		if (c == 27)
+			break;
+	}
+
+	// When everything done, release the video capture object
+	cap.release();
+
+	// closes all the frames
+	cv::destroyAllWindows();
+
 	return 0;
 }
